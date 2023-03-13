e41ad1b981f27a35b70beeee554a055c4f7a7a7a
Vinícius Campos <vinicius.campos@rockcontent.com>
2023-03-13 16:32:47 -0300

    Adding MulterModule

--- src/credit-card/credit-card.module.ts
+++ src/credit-card/credit-card.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CreditCardService } from './credit-card.service';
 import { CreditCardController } from './credit-card.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   imports: [
